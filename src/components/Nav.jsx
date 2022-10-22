217373ada14887c033ff79f014fd17cf62800b88
Jaeha Lee <wogkdkrm112@gmail.com>
2022-10-22 19:50:00 +0900

    swipejs 적용

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -33,7 +33,7 @@ const Nav = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png"
           alt="logo"
           className="nav__logo"
-          onClick={() => window.location.reload()}
+          // onClick={() => window.location.reload()}
         />
       </Link>
       <input
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,17 @@
 import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
-import './Row.css';
 import MovieModal from './MovieModal/MovieModal';
+import './Row.css';
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Row = ({ title, fetchUrl, isLargeRow, id }) => {
   // const BASE_URL = '';
@@ -28,41 +38,53 @@ const Row = ({ title, fetchUrl, isLargeRow, id }) => {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {'<'}
-          </span>
-        </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        // spaceBetween={50}
+        slidesPerView={5}
+        navigation // arrow 버튼
+        pagination={{ clickable: true }} // ... 버튼
+        scrollbar={{ draggable: true }}
+        loop={true} // 자동으로 처음으로 돌아감(무한 반복)
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log('slide change')}
+        breakpoints={{
+          // width px에 따라 다름
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {'>'}
-          </span>
-        </div>
-      </div>
+      </Swiper>
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
